6be4691547430270664d2510c5c8ed1fc3ba1b34
prasenjitdey800 <58329352+prasenjitdey800@users.noreply.github.com>
2020-09-10 07:47:05 +0530

    JSON parsing

---
+++ data.js
@@ -0,0 +1,104 @@
+var data={
+   "title": [
+      "This Page Shows the COVID status of employees of different client companies",
+      "Internal DTD and CSS Styling to XML Respectively"
+   ],
+   "clients_employee_covid_status": [
+      {
+         "client_id": "111",
+         "site": {
+            "@type": "simple",
+            "@href": "https://www.w3schools.com",
+            "#text": "Go to site"
+         },
+         "recov": "23",
+         "conf": "40",
+         "dea": "2",
+         "name": "Q-Analytics",
+         "domain": "Domain: Data Analytics",
+         "confirmed": "Confirmed: 40",
+         "recovered": "Recovered: 23",
+         "deaths": "Deaths: 2"
+      },
+      {
+         "client_id": "112",
+         "site": {
+            "@type": "simple",
+            "@href": "https://www.w3schools.com",
+            "#text": "Go to site"
+         },
+         "recov": "37",
+         "conf": "70",
+         "dea": "10",
+         "name": "Client-hub",
+         "domain": "Domain: Client Engagement",
+         "confirmed": "Confirmed: 70",
+         "recovered": "Recovered: 37",
+         "deaths": "Deaths: 10"
+      },
+      {
+         "client_id": "113",
+         "site": {
+            "@type": "simple",
+            "@href": "https://www.w3schools.com",
+            "#text": "Go to site"
+         },
+         "recov": "39",
+         "conf": "63",
+         "dea": "5",
+         "name": "UI-Pro",
+         "domain": "Domain: UI/UX",
+         "confirmed": "Confirmed: 63",
+         "recovered": "Recovered: 39",
+         "deaths": "Deaths: 5"
+      },
+      {
+         "client_id": "114",
+         "site": {
+            "@type": "simple",
+            "@href": "https://www.w3schools.com",
+            "#text": "Go to site"
+         },
+         "recov": "19",
+         "conf": "39",
+         "dea": "3",
+         "name": "Power-Brainer",
+         "domain": "Domain: Data Analytics",
+         "confirmed": "Confirmed: 39",
+         "recovered": "Recovered: 19",
+         "deaths": "Deaths: 3"
+      },
+      {
+         "client_id": "115",
+         "site": {
+            "@type": "simple",
+            "@href": "https://www.w3schools.com",
+            "#text": "Go to site"
+         },
+         "recov": "47",
+         "conf": "97",
+         "dea": "16",
+         "name": "Phi-Solutions",
+         "domain": "Domain: Data Analytics",
+         "confirmed": "Confirmed: 97",
+         "recovered": "Recovered: 47",
+         "deaths": "Deaths: 16"
+      },
+      {
+         "client_id": "116",
+         "site": {
+            "@type": "simple",
+            "@href": "https://www.w3schools.com",
+            "#text": "Go to site"
+         },
+         "recov": "20",
+         "conf": "50",
+         "dea": "6",
+         "name": "ThinkIT",
+         "domain": "Domain: IT solutions",
+         "confirmed": "Confirmed: 50",
+         "recovered": "Recovered: 20",
+         "deaths": "Deaths: 6"
+      }
+   ]
+}
